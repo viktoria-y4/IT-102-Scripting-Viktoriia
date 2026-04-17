643d4109483635a80b0ec26fdff7acffe50d14bd
viktoria-y4 <yaroshenkko.viktoriia@student.greenriver.edu>
2026-04-17 03:41:35 +0000

    this is me developing a hello script

---
+++ start/hello world.py
@@ -0,0 +1,13 @@
+
+...
+...
+#Promt the user
+#Prompt the user for their name
+guest_name = input("What is your name: ")
+
+#this is to say Hello
+
+print("Hello, ", guest_name)
+
+#Print positive affirimation
+print("Today is going to be a great day. ")
