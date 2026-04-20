c8db49f9ec1cc5868dc5cfe36820c576cca9778d
viktoria-y4 <yaroshenkko.viktoriia@student.greenriver.edu>
2026-04-20 23:43:57 +0000

    Created a new conditional python script

---
+++ start/CH02/Conditional.py
@@ -0,0 +1,10 @@
+...
+...
+
+#This is going to take an input from the user
+answer = input("Is today a good day? (y/n) ").lower()
+
+
+#Its an if statement checking if the string is equal to y and if so print yes it is
+if answer == "y":
+    print("Yes it is")
