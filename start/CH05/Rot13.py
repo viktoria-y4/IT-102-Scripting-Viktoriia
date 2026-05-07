78a659b6620ab1ada2e87703c0f78b47cc98bc4f
viktoria-y4 <yaroshenkko.viktoriia@student.greenriver.edu>
2026-05-07 00:21:05 +0000

    ceasar cilpher

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,76 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Viktoriia Y
+
+def ceasar_encrypt(message: str, shift: int) -> str:
+    """
+    Encrypt a message by shifting each letter a shift acmount
+    A-Z
+    a-z
+
+    """
+
+    result = []
+
+    for char in message:
+        if char.isupper():
+            shifted = (ord(char) - ord("A") + shift) % 26 + ord("A")
+            result.append(chr(shifted))
+        elif char.islower():
+            shifted = (ord(char) - ord("a") + shift) % 26 + ord("a")
+            result.append(chr(shifted))
+        else:
+            result.append(char)
+
+    return "".join(result)
+
+
+def ceasar_decrypt(ciphertext: str, shift: int) -> str:
+    """
+    Decrypt a ceasar cipher message by shifting letters backwards
+    """
+    return ceasar_encrypt(ciphertext, -shift)
+
+def get_shift_value() -> int:
+    """
+    Prompt the user for a amount of shifts to take
+    """
+    while True:
+        try:
+            shift = int(input("Enter a valur between 1-25: "))
+            if 1 <= shift <= 25:
+                return shift
+            print("Please enter a valid shift")
+        except ValueError:
+            print("That is now a vlid number or integer")
+
+def main():
+    print("Ceasar Cipher Encrypt and decrypt")
+    message = input("Enter a message: ").strip()
+    if not message:
+        print("No message entered. Exiting")
+        return
+
+    #get the shift value
+    shift = get_shift_value()   
+
+    #Encrypt
+    encrypted = ceasar_encrypt(message, shift)
+    print(f"Encrypted message: {encrypted}")
+
+    #Decrypted if user even wants to?
+    answer = input("decrypt the message? (yes/no): ").strip().lower()
+
+    if answer in ("yes", "y"):
+        decrypted = ceasar_decrypt(encrypted, shift)
+        print(F"Decrypted message: {decrypted}")
+
+        #confirm
+        match = decrypted == message
+        print("validation of match")
+    else:
+        print("Decryption skipped")
+    print()
+
+if __name__ == "__main__":
+    main()
