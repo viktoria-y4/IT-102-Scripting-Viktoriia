e1505c8ecfee983d4e0e655b8af56d25d1e0c995
viktoria-y4 <yaroshenkko.viktoriia@student.greenriver.edu>
2026-06-03 19:17:57 +0000

    Find code

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,23 @@
-#!/usr/bin/env python3
-# Script that scans web server logs for 404 errors
-# By 
+#Print any IP that gets a 404 status code
+
+"""
+The purpose of this script is to return 404 source IP if its found
+"""
+
+
+#libaries utilized
+import re
+
+def main():
+    LOG_FILE = r"C:\Users\justincase\Desktop\IT.102\IT-102-Spring-2026\start\CH07\access.log"
+    with open(LOG_FILE, "r") as f:
+        for line in f:
+            #Status code which is right after request and first numbers
+            ip = line.split()[0]
+            match = re.search(r'" 404 ', line)
+            if match:
+                print(f"{ip}")
+
+
+if __name__ == "__main__":
+    main()
--- start/CH07/FindStatusCodes.py
+++ start/CH07/FindStatusCodes.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
-# Script that scans web server logs for status codes
-# Use RegEx to find and report on most frequent status messages
-# By 
+
+"""
+This is for counting status codes
+"""
+
+
+#Libaries
+import re
+from collections import defaultdict
+
+
+def main():
+    LOG_FILE = r"C:\Users\justincase\Desktop|IT.102\IT-102-Spring-2026\start\CH07\access.log"
+    counts = defaultdict(int)
+
+    with open (LOG_FILE, "r") as f:
+        for line in f:
+            match = re.search(r'" (\d{3}) ', line)
+            if match:
+                counts[match.group(1)] += 1
+
+
+    for code, count in sorted(counts.items()):
+        print(f"{code}: {count}")
+
+if __name__ == "__main__":
+    main()
